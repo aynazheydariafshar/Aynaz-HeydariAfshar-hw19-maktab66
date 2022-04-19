e8acff070a8a18c77e2c4448db7bf07e9c94396a
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-04-19 18:38:33 +0430

    npm react icon

---
+++ ex1/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return <div>
+        <h1>Where is the World ?</h1>
+        <div>
+            <button>Dark Mode</button>
+        </div>
+    </div>;
+}
+
+
+export default Navbar;
