472e8ac1f1761ec181279da7f2769e849a742cfd
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-04-22 18:24:39 +0430

    get data on context

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,14 +1,19 @@
-import { useContext , useEffect , useState } from 'react';
+import {useEffect , useState } from 'react';
 import './Assests/css/App.css';
 import Country from './Components/Country';
 import Navbar from './Components/Navbar';
 import Search from './Components/Search';
 import { ThemeContext, ThemeProvider } from './Components/ThemeContext';
+import axios from 'axios';
+
 
 function App() {
 
   //change Theme
   const [dark, setDark] = useState(false);
+
+  //data from json file
+  const [dataCountry , setdataCountry] = useState([])
 
   //toggle theme
   const toggleDark = (e, dark2) => {
@@ -16,8 +21,14 @@ function App() {
     setDark(dark)
   }
 
+  //get data from json file
+  useEffect(() => {
+    axios.get('/json/Countries.json')
+     .then(res => setdataCountry(res.data))
+  }, [dataCountry])
+
   return (
-    <ThemeContext.Provider value={{dark , toggleDark}}>
+    <ThemeContext.Provider value={{dark , toggleDark , dataCountry}}>
       <div className={dark ?'light-app' : 'dark-app'}>
         <Navbar />
         <Search />
--- ex1/src/Components/Country.js
+++ ex1/src/Components/Country.js
@@ -1,18 +1,12 @@
-import React, { useState , useEffect} from 'react';
-import axios from 'axios';
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
 
 const Country = () => {
 
-    const [dataCountry , setdataCountry] = useState([])
-
-    //get data from json file
-    useEffect(() => {
-       axios.get('/json/Countries.json')
-        .then(res => setdataCountry(res.data))
-    }, [dataCountry])
+    const dataContext = useContext(ThemeContext)
 
     return <div className='card-style'>
-        {dataCountry.map(item => {
+        {dataContext.dataCountry.map(item => {
             return (<div className='card'>
                 <div className='img-div'>
                     <img src={item.flags.svg} alt='country' className='country-img'/>
