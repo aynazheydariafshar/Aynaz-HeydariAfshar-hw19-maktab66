df1d5e7a1f9fbd55c2012918cc6be4e7d26e3991
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-04-23 20:03:56 +0430

    add text from image on card country

--- ex1/src/Components/CountryCards.js
+++ ex1/src/Components/CountryCards.js
@@ -1,4 +1,4 @@
-import React, { useContext , useEffect} from 'react';
+import React, { useContext , useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import { ThemeContext } from './ThemeContext';
 
@@ -6,23 +6,48 @@ const CountryCards = () => {
 
     const dataContex = useContext(ThemeContext)
     const dataParams = useParams()
+    const [objCountry , setobjCountry] = useState()
 
+    //find country when click on card
     const findCountry = () => {
-       return dataContex.dataCountry.filter(item =>{
-           return item.alpha3Code === dataParams.alphaCode
+        return dataContex.dataCountry.filter(item => {
+            return item.alpha3Code === dataParams.alphaCode
         })
     }
+    
     useEffect(() => {
-        findCountry()
-    }, [])
-    return (
-    <div>
-       <h1>
-           hello
-       </h1>
-       
-    </div>
-    );
+        setobjCountry(findCountry())
+    }, [objCountry])
+
+    return <div className='card-detailes'>
+        {objCountry?.map(item => {
+           return <>
+               <div className='img-card'>
+                   <img src={item.flags.svg}/>
+               </div> 
+               <div className='p-card'>
+                    <div>
+                        <h1>{item.name}</h1>
+                        <p>{`Native Name : ${item.nativeName}`}</p>
+                        <p>{`Population : ${item.population}`}</p>
+                        <p>{`Region : ${item.region}`}</p>
+                        <p>{`Sub Region : ${item.subregion}`}</p>
+                        <p>{`Capital : ${item.capital}`}</p>
+                        <div>
+                            <span>{`Border Countries : `}</span>
+                            {item.borders?.map(item => <span className='borders'>{item} </span>)}
+                        </div>
+                    </div>
+                    <div>
+                        <p>{`Top Level Domain : ${item.topLevelDomain}`}</p>
+                        <p>{`Currencies : ${item.currencies[0].name}`}</p>
+                        <span>Languages :</span>
+                        {item.languages?.map(item => <span>{` ${item.name} ,`}</span>)}
+                    </div>    
+                </div>   
+           </>
+        })}
+    </div>;
 }
 
 export default CountryCards;
